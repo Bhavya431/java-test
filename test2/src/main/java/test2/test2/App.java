55160706e171b689ab1303efccef5cb4e5dac522
Bhavya Garikipati <bhavya.garikipati1@t-mobile.com>
2019-05-04 21:43:07 -0700

    Program for scanner input.

---
+++ test2/src/main/java/test2/test2/App.java
@@ -0,0 +1,19 @@
+package test2.test2;
+
+import java.util.Scanner;
+
+/**
+ * scanner input
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in); 
+    	 System.out.println("Please Enter Name:"); 
+    	  String name = input.next();
+    	  System.out.println("Entered name is "+ name);
+    }
+}
