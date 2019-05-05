d34ab72bd52bf066f27ce9d76a05cffe6f67cbde
Bhavya Garikipati <bhavya.garikipati1@t-mobile.com>
2019-05-05 06:23:43 -0700

    Airthametic Operations using operators by taking input as 2.

---
+++ test5/src/main/java/test5/test5/App.java
@@ -0,0 +1,24 @@
+package test5.test5;
+
+/**
+ * Operations on operators
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a =2;
+    	a += 10;
+        System.out.println( a );
+        a -= 10;
+        System.out.println( a );
+        a *= 5;
+        System.out.println( a);
+        a /= 10;
+        System.out.println( a );
+        a= 31%2;
+        System.out.println( a );
+        
+    }
+}
